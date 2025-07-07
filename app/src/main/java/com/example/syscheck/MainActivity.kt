e5df556c5f9e23473ce7566cb27b80aa4c28220f
Mohit Singh <mohitsingh@3fat.in>
2025-07-08 00:52:32 +0530

    explicit intent

--- app/src/main/java/com/example/syscheck/MainActivity.kt
+++ app/src/main/java/com/example/syscheck/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.syscheck
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.example.syscheck.ui.theme.SysCheckTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +24,6 @@ class MainActivity : ComponentActivity() {
             SysCheckTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +33,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SysCheckTheme {
-        Greeting("Android")
+fun Greeting(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Button(
+        onClick = {
+            val intent = Intent(context, AboutActivity::class.java)
+            context.startActivity(intent)
+        },
+        modifier = Modifier.padding(40.dp)
+    ) {
+        Text(text = "go to about page")
     }
 }
